666371986242ff3c959c1cc3dab7b2ae018eac8b
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-05-29 17:35:31 +0300

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-export default function Footer() {
-    return <div className="stats">
-      <h5>Footer</h5>
-  </div>;
+export default function Footer({ items }) {
+  let totalItems = 0;
+  let packedItems = 0;
+  
+
+  items.map((item) => {
+    totalItems += Number(item.counter);
+    if (item.packed) {
+      packedItems += Number(item.counter);
+    }
+  });
+    
+    
+  const percentPackedItems = totalItems > 0 ? ((packedItems / totalItems) * 100).toFixed(0) : 0;
+
+
+  return (
+    <div className="stats">
+      <h5>You have {totalItems} items in your list</h5>
+      <h6>
+        You already packet {packedItems} items {`${percentPackedItems}%`}
+      </h6>
+    </div>
+  );
 }
