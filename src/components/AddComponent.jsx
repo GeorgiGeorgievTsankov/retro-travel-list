617d57e95145589db041aa51e6cadcd0ee395d26
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-05-29 14:50:58 +0300

    Add files via upload

--- src/components/AddComponent.jsx
+++ src/components/AddComponent.jsx
@@ -4,18 +4,6 @@ export default function AddComponent({ onAddItem }) {
   const [newItem, setNewItem] = useState("");
   const [optCount, setOptCount] = useState(1);
 
-  const handleChange = (e) => {
-    setNewItem(e.target.value);
-  };
-
-  const handleAdd = () => {
-    if (newItem.trim() !== "") {
-      const newData = { id: Date.now(), name: newItem, counter: optCount };
-      onAddItem(newData);
-      setNewItem("");
-    }
-  };
-
   const count = 20;
 
   function counter(count) {
@@ -34,18 +22,32 @@ export default function AddComponent({ onAddItem }) {
     setOptCount(e.target.value);
   }
 
+  const handleChange = (e) => {
+    setNewItem(e.target.value);
+  };
+
+  const handleAdd = () => {
+    if (newItem.trim() !== "") {
+      const newData = { id: Date.now(), name: newItem, counter: optCount };
+      onAddItem(newData);
+      setNewItem("");
+      setOptCount(1);
+    }
+  };
+
   return (
     <div className="add-form">
       <h2>What do you need for your trip?</h2>
       <div className="units">
         <h3>Units: </h3>
-        <select onChange={handleChangeOption}>{counter(count)}</select>
+              <select onChange={handleChangeOption}  value={optCount}>{counter(count)}</select>
       </div>
       <input
         type="text"
         placeholder="Items..."
         value={newItem}
         onChange={handleChange}
+        maxLength={18}
       />
       <button onClick={handleAdd}>ADD</button>
     </div>
--- src/components/LIstComponent.jsx
+++ src/components/LIstComponent.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 
-export default function LIstComponent({ startList }) {
+export default function LIstComponent({ startList, onDeleteItem }) {
   const data = startList;
-  console.log(startList);
+//   console.log(startList);
 
   return (
     <div className="list">
       {data.map((element, index) => (
-        <div className="element-box" key={index}>
-          <p>{element.counter} ps.</p>
-          <p>{element.name}</p>
-          <button>X</button>
+        <div className="element-box" key={element.id}>
+          <div className="list-checkbox">
+            <input
+              type="checkbox"
+              id="myCheckbox"
+              name="myCheckbox"
+              value="value"
+            ></input>
+            <p>{element.counter} </p>
+          </div>
+          <p className={element.name.length > 16 ? 'small-font' : ''}>
+            {element.name}
+          </p>
+          <button onClick={ () => onDeleteItem(element.id) }>❌</button>
         </div>
       ))}
-    </div>
+    </div>  
   );
 }
