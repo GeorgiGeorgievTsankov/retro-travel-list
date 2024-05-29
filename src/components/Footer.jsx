bafcf98dbdb7394817391ab94e98864a1f54abae
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-05-29 17:54:43 +0300

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function Footer({ items }) {
   let totalItems = 0;
   let packedItems = 0;
-  
 
   items.map((item) => {
     totalItems += Number(item.counter);
@@ -11,17 +10,29 @@ export default function Footer({ items }) {
       packedItems += Number(item.counter);
     }
   });
-    
-    
-  const percentPackedItems = totalItems > 0 ? ((packedItems / totalItems) * 100).toFixed(0) : 0;
 
+  const percentPackedItems =
+        totalItems > 0 ? ((packedItems / totalItems) * 100).toFixed(0) : 0;
+    
+        if (totalItems <= 0)
+            return (
+              <div className="stats">
+                <em>Start adding some items to your packing list 🚀</em>
+              </div>
+            );
 
-  return (
-    <div className="stats">
-      <h5>You have {totalItems} items in your list</h5>
-      <h6>
-        You already packet {packedItems} items {`${percentPackedItems}%`}
-      </h6>
-    </div>
-  );
+    return (
+        percentPackedItems == 100 ? (
+          <div className="stats">
+            <h5>You got everything! Ready to go ✈️</h5>
+          </div>
+        ) : (
+          <div className="stats">
+            <h5>You have {totalItems} items in your list</h5>
+            <h6>
+              You already packed {packedItems} items ({`${percentPackedItems}%`})
+            </h6>
+          </div>
+        )
+      );
 }
